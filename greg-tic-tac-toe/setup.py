c187dcde208ed7af80e58a6f552de84ecf3711f7
GregMeno <greg.meno@gmail.com>
2010-06-01 08:33:20 -0400

    added a distutils script and wrapped up in a debian package, added a simple gtk visual of board, TODO install glade file and make UI playable.

---
+++ greg-tic-tac-toe/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='tic-tac-toe',
+      version = "1.0",
+      author = "Greg Meno",
+      author_email = "greg.meno@gmail.com",
+      scripts=["greg-tic-tac-toe"])
