7107adcd162d5c44df232c24ed0b63a7480b847f
GregMeno <greg.meno@gmail.com>
2010-05-31 21:14:44 -0400

    Some tests to keep me honest. Looks like choose move does have bugs.

---
+++ tests.py
@@ -0,0 +1,38 @@
+import tic
+
+def test_has_won():
+    #TEST ODDS and EVENS in has_won()
+    for x in range(tic.NUMBER_OF_MOVES):
+        current_state = tuple(xrange(x))
+        tic.has_won(current_state)
+
+def choose_all_moves(current_state, start_game=0):
+    remaining_moves = tuple(x for x in range(tic.NUMBER_OF_MOVES) if x not in current_state) 
+    
+    #if the computer wins first dont keep looking at this state
+    my_moves = tuple(current_state[i] for i in range(start_game, len(current_state), 2))
+    comp_moves = tuple(x for x in current_state if x not in my_moves) 
+    if tic.player_won(comp_moves):
+        return 
+ 
+    #base case check
+    if len(current_state) == tic.NUMBER_OF_MOVES:
+        if tic.player_won(my_moves):
+            print 'OOPS', current_state, my_moves
+
+    #we play the opening move? 
+    if len(current_state) % 2 == start_game:
+        for next_move in remaining_moves:
+            choose_all_moves(current_state + (next_move,))
+    else:
+        choose_all_moves(current_state + (tic.choose_move(current_state),)) 
+            
+            
+        
+        
+if __name__ == '__main__':
+    current_state = ()
+    print 'losing games where Human goes first'
+    choose_all_moves(current_state)
+    print 'losing games where Computer goes first'
+    choose_all_moves(current_state, 1)
